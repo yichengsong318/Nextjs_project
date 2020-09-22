04310df7b991e1d67847c27aa5429f5bfd45d637
win <fancodeexpert@outlook.com>
2020-09-23 07:41:39 +0800

    upload image function

--- components/pageSection/profile/BasicProfile.jsx
+++ components/pageSection/profile/BasicProfile.jsx
@@ -20,15 +20,28 @@ const getUserDetails = async () => {
 
 const BasicProfile = () => {
   const dispatch = useDispatch();
-  const [currentItem, setCurrentItem] = useState("")
-  const [currentSubPage, setCurrentSubPage] = useState()
-  const [activeList, setActiveList] = useState([])
-  const [userDetails, setUserDetails] = useState()
+  const [currentItem, setCurrentItem] = useState("");
+  const [currentSubPage, setCurrentSubPage] = useState();
+  const [activeList, setActiveList] = useState([]);
+  const [userDetails, setUserDetails] = useState();
+  const [avatar, setAvatar] = useState("");
   const { branchName, id: branchId } = useSelector((state) => state.root.currentBranch);
 
+  const uploadImage = async (formData) => {
+    try {
+      const url = `customer/web/profile-service/me/avatar`;
+      const res = await axios.post(url, formData)
+      _process()
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
   const _process = async () => {
-    const userDetail = await getUserDetails()
-    setUserDetails(userDetail)
+    const userDetail = await getUserDetails();
+    setAvatar(userDetail.avatar)
+    setUserDetails(userDetail);
   }
 
   useEffect(() => {
@@ -44,6 +57,17 @@ const BasicProfile = () => {
   const onLogout = () => {
     // dispatch(logOut());
     localStorage.removeItem("user")
+  }
+
+  const onUploadImage = (e) => {
+    let file = e.target.files[0];
+    const formData = new FormData();
+    formData.append(
+      "avatar",
+      file,
+      file.name
+    );
+    uploadImage(formData)
   }
 
   useEffect(() => {
@@ -77,10 +101,10 @@ const BasicProfile = () => {
             <div class="col-md-4">
               <div class="order-left">
                 <div class="user-box">
-                  <div class="text-center display-avata"><span class="img-circle"><img src="/images/picture/avata-16.jpg" alt="" title="" /> </span></div>
+                  <div class="text-center display-avata"><span class="img-circle"><img src={avatar} alt="" title="" /> </span></div>
                   <div class="text-center">
                     <label class="upload-avata relative">
-                      <input type="file" class="hide-abs" />
+                      <input type="file" class="hide-abs" onChange={onUploadImage} accept="image/*" />
                       <span><i class="fa fa-upload"></i> Upload</span>
                     </label>
                   </div>
